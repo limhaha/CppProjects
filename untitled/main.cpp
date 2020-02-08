b4370bf71cd04a688e27c93d38712c877a58ffd5
HALIM_LEE <rimha93@gmail.com>
2020-02-08 19:08:01 +0900

    test

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -2,13 +2,13 @@
 
 using namespace std;
 
-int main(){
-    using salary = double;
-    salary peter = 100.12;
-    salary &sangwon = peter;
-    cout << "peter's memory address = " << &peter << endl;
-    cout << "sangwon's memory address = " << &sangwon << endl;
-    return 0;
+int main() {
+    int i;
+    int arr[10];
+    for(int i=0; i<10; i++){
+        arr[i] = i * 2;
+    }
+    int *ptr = arr;
+    cout << arr[3] << endl;
+    cout << *(ptr+3) << endl;
 }
-
-const float PI = 3.1415926;
