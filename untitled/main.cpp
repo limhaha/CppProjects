b498c05013787d472c36b82d95dcded9fb6ef263
HALIM_LEE <rimha93@gmail.com>
2020-02-17 18:57:54 +0900

    백준 기본문제

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -2,13 +2,14 @@
 
 using namespace std;
 
-int main() {
-    int i;
-    int arr[10];
-    for(int i=0; i<10; i++){
-        arr[i] = i * 2;
-    }
-    int *ptr = arr;
-    cout << arr[3] << endl;
-    cout << *(ptr+3) << endl;
+int main(){
+    int a,b;
+
+    cin >> a >> b ;
+    cout << a * (b%10) << endl;
+    cout << a * ((b/10) % 10) << endl;
+    cout << a * (b/100) << endl;
+    cout << a * b <<endl;
+    return 0;
 }
+
