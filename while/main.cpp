6a8439de41c4c87a2c5aaf9972bc44116ff2a450
HALIM_LEE <rimha93@gmail.com>
2020-03-03 23:07:48 +0900

    .

--- while/main.cpp
+++ while/main.cpp
@@ -1,40 +1,40 @@
 #include <iostream>
 
 using namespace std;
-
-int main() {
-    int a;
-    int count = 0;
-    int n = 0;
-    cin >> a;
-    n = a;
-    if (a < 10) {
-        n = n * 10;
-    }
-    a = n;
-    while (1) {
-        n = ((n % 10) * 10) + (n / 10 + n % 10)%10;
-        count++;
-        if (n == a) {
-            break;
-
-        }
-    }
-    cout << count << endl;
-    return 0;
-}
-
-//using namespace std;
-//
-//const int MAX = 1000000;
 //
 //int main() {
-//    int n;
-//    cin >> n;
-//    int input = 0;
-//    int min = MAX;
-//    int max = -MAX;
+//    int a;
+//    int count = 0;
+//    int n = 0;
+//    cin >> a;
+//    n = a;
+//    if (a < 10) {
+//        n = n * 10;
+//    }
+//    a = n;
+//    while (1) {
+//        n = ((n % 10) * 10) + (n / 10 + n % 10)%10;
+//        count++;
+//        if (n == a) {
+//            break;
 //
-//    for(int i=0; i<n; i++){
-//
+//        }
+//    }
+//    cout << count << endl;
+//    return 0;
 //}
+
+using namespace std;
+
+const int MAX = 1000000;
+
+int main() {
+    int n;
+    cin >> n;
+    int input = 0;
+    int min = MAX;
+    int max = -MAX;
+
+    for(int i=0; i<n; i++){
+
+}
