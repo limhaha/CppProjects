d63c902291b967d11611095f48a8577b07a36748
HALIM_LEE <rimha93@gmail.com>
2020-04-22 17:20:55 +0900

    avl tree

---
+++ AVLtree/cmake-build-debug/CMakeFiles/AVLtree.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "AVLtree"
+  "AVLtree.pdb"
+  "CMakeFiles/AVLtree.dir/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/AVLtree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
