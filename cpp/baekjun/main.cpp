95dfa7f2f0a630e9ecc7825010e51ef43668ddcf
HALIM_LEE <rimha93@gmail.com>
2020-05-07 01:40:27 +0900

    .

--- cpp/baekjun/main.cpp
+++ cpp/baekjun/main.cpp
@@ -3,14 +3,37 @@
 #include <string>
 #include <vector>
 
+
+#define MAX 10001
+
 using namespace std;
+int d(int);
 
-long long sum(vector<int> &a){
-    long long ans = 0;
+int main(){
+    vector<bool> selfAry;
+    selfAry.assign(MAX, true);
+    int notSelfNum;
 
-    for(vector<int>::iterator itr = a.begin(); itr!= a.end(); itr++){
-        ans += *itr;
+    for(int i=0; i<MAX; i++){
+        notSelfNum = d(i);
+        if(notSelfNum>MAX)
+            continue;
+        selfAry[notSelfNum] = false;
+    }
+    for(int i=0; i<MAX; i++){
+        if(selfAry[i]){
+            cout << i <<endl;
+        }
     }
 
-    return ans;
+    return 0;
+}
+
+int d(int n){
+    int num = n;
+    do{
+        num += n % 10;
+    } while ((n/=10) != 0);
+
+    return num;
 }
