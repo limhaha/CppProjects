abc8d13560f7c7a1b82b9481b49f2f75fc476fd6
HALIM_LEE <rimha93@gmail.com>
2020-06-16 22:37:35 +0900

    PHW5-2

--- 자료구조/PHW5-2/main.c
+++ 자료구조/PHW5-2/main.c
@@ -6,17 +6,24 @@
 #define MAX 100
 #define SWAP(i, j, t) t = i; i = j; j = t;
 
-void selection_sort(int[], int);
+void selection_sort(int arr[], int n);
 void quick_sort(int arr[], int left, int right);
+void join(int t1c[], int t2c[], int val);
 
 struct table {
     int columns[3];
 };
 
+struct mergetable{
+    int coulmns[5];
+};
+
+struct table t1[15];
+struct table t2[10];
+
 int main() {
 
-    struct table t1[15];
-    struct table t2[10];
+
 
     srand(time(NULL));
     int random;
@@ -28,8 +35,6 @@ int main() {
         }
     }
 
-    t1[9].columns[2] = 55;
-    t1[13].columns[2] = 55;
 
     for(int i=0; i<10; i++){
         for(int j=0; j<3; j++){
@@ -38,7 +43,6 @@ int main() {
         }
     }
 
-    t2[4].columns[0] = 55;
 
     printf("<TABLE 1>\n");
     printf("----------------\n");
@@ -66,53 +70,89 @@ int main() {
 
     printf("\n\n");
 
-//    int selection1[15];
-//    for(int i=0; i<15; i++){
-//        selection1[i] = t1[i].columns[2];
-//    }
-//    selection_sort(selection1, 15);
-//    for(int i = 0 ; i < 15; i ++) {
-//        t1[i].columns[2] = selection1[i];
-//    }
-//
-//    printf("Duplex Selction Sort \n<TABLE 1 on Column3>\n");
-//    printf("----------------\n");
-//    for(int i=0; i<15; i++){
-//        for(int j=0; j<3; j++){
-//            printf("|%3d ", t1[i].columns[j]);
-//        }
-//        printf("|");
-//        printf("\n");
-//        printf("----------------\n");
-//    }
-//
-//    printf("\n\n");
-//
-//    int selection2[10];
-//    for(int i=0; i<10; i++){
-//        selection2[i] = t2[i].columns[0];
-//    }
-//    selection_sort(selection2, 10);
-//    for(int i = 0 ; i < 10; i ++) {
-//        t2[i].columns[0] = selection2[i];
-//    }
-//
-//    printf("Duplex Selction Sort \n<TABLE 2 on Column1>\n");
-//    printf("----------------\n");
-//    for(int i=0; i<10; i++){
-//        for(int j=0; j<3; j++){
-//            printf("|%3d ", t2[i].columns[j]);
-//        }
-//        printf("|");
-//        printf("\n");
-//        printf("----------------\n");
-//    }
+    t1[9].columns[2] = 55;
+    t1[13].columns[2] = 55;
+    t2[4].columns[0] = 55;
+
+
+    //Print After Update
+    printf("<TABLE 1>\n");
+    printf("----------------\n");
+    for(int i=0; i<15; i++){
+        for(int j=0; j<3; j++){
+            printf("|%3d ", t1[i].columns[j]);
+        }
+        printf("|");
+        printf("\n");
+        printf("----------------\n");
+    }
+
+    printf("\n\n");
+
+    printf("<TABLE 2>\n");
+    printf("----------------\n");
+    for(int i=0; i<10; i++){
+        for(int j=0; j<3; j++){
+            printf("|%3d ", t2[i].columns[j]);
+        }
+        printf("|");
+        printf("\n");
+        printf("----------------\n");
+    }
+
+    printf("\n\n");
+
+
+    int selection1[15];
+    for(int i=0; i<15; i++){
+        selection1[i] = t1[i].columns[2];
+    }
+    selection_sort(selection1, 15);
+    for(int i = 0 ; i < 15; i ++) {
+        t1[i].columns[2] = selection1[i];
+    }
+
+    printf("Duplex Selction Sort \n<TABLE 1 on Column3>\n");
+    printf("----------------\n");
+    for(int i=0; i<15; i++){
+        for(int j=0; j<3; j++){
+            printf("|%3d ", t1[i].columns[j]);
+        }
+        printf("|");
+        printf("\n");
+        printf("----------------\n");
+    }
+
+    printf("\n\n");
+
+    int selection2[10];
+    for(int i=0; i<10; i++){
+        selection2[i] = t2[i].columns[0];
+    }
+    selection_sort(selection2, 10);
+    for(int i = 0 ; i < 10; i ++) {
+        t2[i].columns[0] = selection2[i];
+    }
+
+    printf("Duplex Selction Sort \n<TABLE 2 on Column1>\n");
+    printf("----------------\n");
+    for(int i=0; i<10; i++){
+        for(int j=0; j<3; j++){
+            printf("|%3d ", t2[i].columns[j]);
+        }
+        printf("|");
+        printf("\n");
+        printf("----------------\n");
+    }
+
+    printf("\n\n");
 
     int quick1[15];
     for(int i=0; i<15; i++){
         quick1[i] = t1[i].columns[2];
     }
     quick_sort(quick1, 0, 15-1);
+
     for(int i = 0 ; i < 15; i ++) {
         t1[i].columns[2] = quick1[i];
     }
@@ -150,79 +190,108 @@ int main() {
         printf("\n");
         printf("----------------\n");
     }
+
+    printf("\n\n");
+
+    join(quick1, quick2, 55 );
+
     return 0;
 }
 
-void selection_sort(int num[], int n){
-    int left = 0;
-    int right = n-1;
-    int minIdx = -1, maxIdx = -1, min = MAX, max = MIN;
-    int tmp;
+void selection_sort(int arr[], int n){
 
-    while(left < right){
-        for(int i = left; i <= right; i++){
-            if(max < num[i]){
-                maxIdx = i;
-                max = num[i];
+    int tmp;
+    for (int i=0, j=n-1; i<j; i++, j--) {
+        int min = arr[i], max = arr[i];
+        int min_i = i, max_i = i;
+        for (int k = i; k <= j; k++)  {
+            if (arr[k] > max) {
+                max = arr[k];
+                max_i = k;
+            } else if (arr[k] < min) {
+                min = arr[k];
+                min_i = k;
             }
-            if(min > num[i]){
-                minIdx = i;
-                min = num[i];
-            }
         }
-        if(maxIdx == left && minIdx == right){
-            SWAP(num[right], num[maxIdx], tmp)
+
+        SWAP(arr[i], arr[min_i], tmp)
+
+        if (arr[min_i] == max) {
+            SWAP(arr[j], arr[min_i], tmp)
         }
-        else{
-            SWAP(num[right], num[maxIdx], tmp)
-            SWAP(num[left], num[minIdx], tmp)
+        else {
+            SWAP(arr[j], arr[max_i], tmp)
         }
-        min = MAX;
-        max = MIN;
-        left++;
-        right--;
     }
 }
 
-void Swap(int arr[], int a, int b) // a,b 스왑 함수
-{
+void Swap(int arr[], int a, int b){
     int temp = arr[a];
     arr[a] = arr[b];
     arr[b] = temp;
 }
-int Partition(int arr[], int left, int right)
-{
-    int pivot = arr[left]; // 피벗의 위치는 가장 왼쪽에서 시작
+
+int Partition(int arr[], int left, int right){
+    int pivot = arr[left];
     int low = left + 1;
     int high = right;
 
-    while (low <= high) // 교차되기 전까지 반복한다
-    {
-        while (low <= right && pivot >= arr[low]) // 피벗보다 큰 값을 찾는 과정
-        {
-            low++; // low를 오른쪽으로 이동
+    while (low <= high){
+        while (low <= right && pivot >= arr[low]){
+            low++;
         }
-        while (high >= (left+1)  && pivot <= arr[high]) // 피벗보다 작은 값을 찾는 과정
-        {
-            high--; // high를 왼쪽으로 이동
+        while (high >= (left+1)  && pivot <= arr[high]){
+            high--;
         }
-        if (low <= high)// 교차되지 않은 상태이면 스왑 과정 실행
-        {
-            Swap(arr, low, high); //low와 high를 스왑
+        if (low <= high){
+            Swap(arr, low, high);
         }
     }
-    Swap(arr, left, high); // 피벗과 high가 가리키는 대상을 교환
-    return high;  // 옮겨진 피벗의 위치정보를 반환
-
+    Swap(arr, left, high);
+    return high;
 }
 
 
-void quick_sort(int arr[], int left, int right)
-{
-    if (left <= right)
-    {
-        int pivot = Partition(arr, left, right); // 둘로 나누어서
-        quick_sort(arr, left, pivot - 1); // 왼쪽 영역을 정렬한다.
-        quick_sort(arr, pivot + 1, right); // 오른쪽 영역을 정렬한다.
+void quick_sort(int arr[], int left, int right){
+    if (left <= right){
+        int pivot = Partition(arr, left, right);
+        quick_sort(arr, left, pivot - 1);
+        quick_sort(arr, pivot + 1, right);
+    }
+}
+
+void join(int t1c[], int t2c[], int val){
+    struct mergetable mt[2];
+    int n=0;
+    int i, j, k;
+
+    for(i=0; i<15; i++){
+        if(t1c[i] == val) {
+//            printf("%d\n", i);
+            for (j=0; j<10; j++) {
+                if(t2c[j] == val){
+//                    printf("%d\n", j);
+                    for(k=0; k<3; k++){
+                        mt[n].coulmns[k] = t1[i].columns[k];
+                    }
+                    for(k=3; k<5; k++){
+                        mt[n].coulmns[k] = t2[j].columns[k-2];
+                    }
+                    n = n+ 1;
+                }
+            }
+        }
+    }
+
+
+    printf("Merge Join of the two tables\n");
+    printf("--------------------------\n");
+    for(int i=0; i<2; i++){
+        for(int j=0; j<5; j++){
+            printf("|%3d ", mt[i].coulmns[j]);
+        }
+        printf("|");
+        printf("\n");
+        printf("--------------------------\n");
     }
 }
